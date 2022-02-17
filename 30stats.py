955e334372eacfd0c3d8998e434912553501c3da
Tu Lab <itucker@ucdavis.edu>
2022-02-16 17:47:24 -0800

    homework

---
+++ 30stats.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+# Write a program that computes typical stats
+# Count, Min, Max, Mean, Std. Dev, Median
+# No, you cannot import the stats library!
+
+import sys
+import math
+
+print(sys.argv)
+numeros = []
+cont = 0
+for num in sys.argv[1:]:
+	numeros.append(int(num))
+numeros.sort()
+print(f'minimum: {numeros[0]}')
+for i in numeros:
+	cont += 1
+print(f'count: {cont}')
+print(f'maximum: {numeros[4]}')
+n = len(numeros)
+mean = sum(numeros) / n
+print(f'mean:{mean}')
+stdev = [(x - mean) ** 2 for x in numeros]#list comprehension
+varab = sum(stdev) / n
+sqt = varab**0.5
+print(f'Std.dev:{sqt}')
+midpoint= (n-1)//2
+if midpoint % 2:
+	print(numeros[midpoint]) 
+else:
+	print(f'median:{numeros[midpoint] + numeros[midpoint + 1] / 2.0}')
+"""
+python3 30stats.py 3 1 4 1 5
+Count: 5
+Minimum: 1.0
+Maximum: 5.0
+Mean: 2.800
+Std. dev: 1.600
+Median 3.000
+"""
